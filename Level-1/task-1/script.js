5aa6658d670d0dfcd917b253941ed0bb7ca828dd
puskar2023 <puskarmandal04@gmail.com>
2026-01-10 20:45:13 +0530

    first commit

---
+++ Level-1/task-1/script.js
@@ -0,0 +1,63 @@
+
+  
+  
+  const sections = document.querySelectorAll("section[id]");
+  const navLinksAll = document.querySelectorAll(".nav-link");
+
+  function activateNavLink() {
+    let scrollY = window.scrollY + 150; // offset for fixed navbar
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const sectionId = section.getAttribute("id");
+
+      if (
+        scrollY >= sectionTop &&
+        scrollY < sectionTop + sectionHeight
+      ) {
+        navLinksAll.forEach(link => {
+          link.classList.remove("active");
+          if (link.getAttribute("href") === `#${sectionId}`) {
+            link.classList.add("active");
+          }
+        });
+      }
+    });
+  }
+
+  window.addEventListener("scroll", activateNavLink);
+
+  
+  
+  
+  
+  
+  
+  const navbar = document.getElementById("navbar");
+  const menuBtn = document.getElementById("menuBtn");
+  const navLinks = document.getElementById("navLinks");
+
+  let lastScrollY = window.scrollY;
+
+  // hide / show navbar on scroll
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > lastScrollY) {
+      navbar.classList.add("nav-hide");
+    } else {
+      navbar.classList.remove("nav-hide");
+    }
+    lastScrollY = window.scrollY;
+  });
+
+  // toggle mobile menu
+  menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
+  // close menu after click
+  document.querySelectorAll("#navLinks a").forEach(link => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("active");
+    });
+  });
